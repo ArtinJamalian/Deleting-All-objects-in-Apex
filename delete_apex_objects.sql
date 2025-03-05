813f7c308a362aa24c4cbbd849516fa872392b69
BluArt <105598027+ArtinJamalian@users.noreply.github.com>
2025-03-05 10:52:45 +0330

    Add files via upload

---
+++ delete_apex_objects.sql
@@ -0,0 +1,20 @@
+begin 
+    for a in (select table_name from object_users) 
+    loop
+        execute immediate 'alter table "'|| a.table_name || '" disable all triggers';
+    end loop;
+
+    for obj in (select object_name, object_type from user_objects order by object_type desc)
+    loop
+        begin 
+            if obj.object_type = 'TABLE' then 
+                execute immediate 'DROP ' || obj.object_type || ' "' || obj.object_name || '" CASCADE CONSTRAINTS';
+            else 
+                execute immediate 'DROP ' || obj.object_type || ' "' || obj.object_name || '" FORCE';
+            end if;
+        exception 
+            when others then 
+                null; -- Optional: log or handle the error
+        end;
+    end loop;
+end;
